b9885ed5584b598649d4af4419fb2d037f25cba0
ediz1234 <balakchiev09@gmail.com>
2025-01-06 17:34:35 +0200

    SimpleStoreg add to save persons

--- Solidity Smart Contract Development/simpleStoreg/SimpleStorage.sol
+++ Solidity Smart Contract Development/simpleStoreg/SimpleStorage.sol
@@ -3,14 +3,26 @@ pragma solidity 0.8.19; //  solidity versions
 
 contract SimpleStorege {
     //favoriteNumber
-    uint256 public favoriteNumber;
+    uint256 internal myFavoriteNumber;
+    // uint256[] listOfFavoriteNumbers;
+
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+    // dynamic array
+    Person[] public listOfPeople; // [] emty list
+    //  Person public pat = Person({favoriteNumber: 7, name: "Pat"});
 
     function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
-        retrieve();
+        myFavoriteNumber = _favoriteNumber;
     }
+
     //view , pure
     function retrieve() public view returns (uint256) {
-        return favoriteNumber;
+        return myFavoriteNumber;
+    }
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
     }
 }
